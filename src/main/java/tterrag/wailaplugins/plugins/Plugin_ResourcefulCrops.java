a00ada5eee5606de798771eae79c3b2eb5147b9c
tterrag1098 <tterrag1098@gmail.com>
2015-08-16 02:58:09 -0400

    Update RCrops plugin

--- src/main/java/tterrag/wailaplugins/plugins/Plugin_ResourcefulCrops.java
+++ src/main/java/tterrag/wailaplugins/plugins/Plugin_ResourcefulCrops.java
@@ -1,8 +1,5 @@
 package tterrag.wailaplugins.plugins;
 
-import com.enderio.core.client.render.RenderUtil;
-import cpw.mods.fml.relauncher.Side;
-import cpw.mods.fml.relauncher.SideOnly;
 import mcp.mobius.waila.api.IWailaBlockDecorator;
 import mcp.mobius.waila.api.IWailaConfigHandler;
 import mcp.mobius.waila.api.IWailaDataAccessor;
@@ -12,15 +9,19 @@ import net.minecraft.entity.item.EntityItem;
 import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.Vec3;
-import tehnut.resourceful.crops.base.Seed;
+import tehnut.resourceful.crops.api.base.Seed;
+import tehnut.resourceful.crops.api.registry.SeedRegistry;
 import tehnut.resourceful.crops.block.BlockRCrop;
 import tehnut.resourceful.crops.registry.ItemRegistry;
-import tehnut.resourceful.crops.registry.SeedRegistry;
 import tehnut.resourceful.crops.tile.TileRCrop;
 import tehnut.resourceful.crops.util.Utils;
 
+import com.enderio.core.client.render.RenderUtil;
+
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+
 import static org.lwjgl.opengl.GL11.*;
-import static org.lwjgl.opengl.GL11.glPopMatrix;
 
 public class Plugin_ResourcefulCrops extends PluginBase implements IWailaBlockDecorator {
 
